71d58dd160ddfd9f1886f8d945eb36d3a73a4073
neuecc <ils@neue.cc>
2021-04-16 12:33:21 +0900

    DisposableBag

---
+++ src/MessagePipe/DisposableBag.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MessagePipe
+{
+    public static partial class DisposableBag
+    {
+        public static IDisposable Create(params IDisposable[] disposables)
+        {
+            return new NthDisposable(disposables);
+        }
+
+        sealed class NthDisposable : IDisposable
+        {
+            bool disposed;
+            readonly IDisposable[] disposables;
+
+            public NthDisposable(IDisposable[] disposables)
+            {
+                this.disposables = disposables;
+            }
+
+            public void Dispose()
+            {
+                if (!disposed)
+                {
+                    disposed = true;
+                    foreach (var item in disposables)
+                    {
+                        item.Dispose();
+                    }
+                }
+            }
+        }
+
+        public static DisposableBagBuilder CreateBuilder()
+        {
+            return new DisposableBagBuilder();
+        }
+
+        public static DisposableBagBuilder CreateBuilder(int initialCapacity)
+        {
+            return new DisposableBagBuilder(initialCapacity);
+        }
+    }
+
+    public partial class DisposableBagBuilder
+    {
+        readonly List<IDisposable> disposables;
+
+        internal DisposableBagBuilder()
+        {
+            disposables = new List<IDisposable>();
+        }
+
+        internal DisposableBagBuilder(int initialCapacity)
+        {
+            disposables = new List<IDisposable>(initialCapacity);
+        }
+
+        public void Add(IDisposable disposable)
+        {
+            disposables.Add(disposable);
+        }
+
+        //public IDisposable Build() in Disposables.tt(Disposables.cs)
+
+        class NullDisposable : IDisposable
+        {
+            public static IDisposable Instance = new NullDisposable();
+
+            NullDisposable()
+            {
+
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
