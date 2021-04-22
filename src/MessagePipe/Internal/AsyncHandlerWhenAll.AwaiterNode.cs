b9cb51645c109c29269eb44244e2256977a807cb
neuecc <ils@neue.cc>
2021-04-22 10:53:42 +0900

    use PoolStack

---
+++ src/MessagePipe/Internal/AsyncHandlerWhenAll.AwaiterNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
+namespace MessagePipe.Internal
+{
+    internal partial class AsyncHandlerWhenAll<T>
+    {
+        internal class AwaiterNode : IPoolStackNode<AwaiterNode>
+        {
+            public AwaiterNode? NextNode { get; set; }
+
+            AsyncHandlerWhenAll<T> parent = default!;
+            ValueTaskAwaiter awaiter;
+
+            readonly Action continuation;
+
+            static PoolStack<AwaiterNode> pool;
+
+            public AwaiterNode()
+            {
+                this.continuation = OnCompleted;
+            }
+
+            public static void RegisterUnsafeOnCompleted(AsyncHandlerWhenAll<T> parent, ValueTaskAwaiter awaiter)
+            {
+                if (!pool.TryPop(out var result))
+                {
+                    result = new AwaiterNode();
+                }
+                result!.parent = parent;
+                result.awaiter = awaiter;
+
+                result.awaiter.UnsafeOnCompleted(result.continuation);
+            }
+
+            void OnCompleted()
+            {
+                var p = this.parent;
+                var a = this.awaiter;
+                this.parent = null!;
+                this.awaiter = default;
+
+                pool.TryPush(this);
+
+                try
+                {
+                    a.GetResult();
+                }
+                catch (Exception ex)
+                {
+                    p.exception = ExceptionDispatchInfo.Capture(ex);
+                    p.TryInvokeContinuation();
+                    return;
+                }
+
+                p.IncrementSuccessfully();
+            }
+        }
+    }
+}
