4a55b0e586c71f76225608f58f6e806f3810c941
neuecc <ils@neue.cc>
2021-05-08 17:47:17 +0900

    Add Analyzer

---
+++ sandbox/MessagePipe.AnalyzerTestApp/Program.cs
@@ -0,0 +1,206 @@
+﻿#pragma warning disable CS1998
+#pragma warning disable MPA001 // for check MessagePipe.Analyzer, remove this line.
+
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace MessagePipe.AnalyzerTestApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable.Range(1, 10).Subscribe(x => Console.WriteLine("OK Rx"));
+        }
+
+        static IDisposable Ret(ISubscriber<int> subscriber)
+        {
+            return subscriber.Subscribe(x => Console.WriteLine("OK RETURN"));
+        }
+
+        static IDisposable Ret(ISubscriber<int, int> subscriber)
+        {
+            return subscriber.Subscribe(0, x => Console.WriteLine("OK RETURN"));
+        }
+
+        static IDisposable Ret(IAsyncSubscriber<int> subscriber)
+        {
+            return subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK RETURN"));
+        }
+
+        static IDisposable Ret(IAsyncSubscriber<int, int> subscriber)
+        {
+            return subscriber.Subscribe(0, async (x, ct) => Console.WriteLine("OK RETURN"));
+        }
+
+        static IDisposable Ret(IBufferedSubscriber<int> subscriber)
+        {
+            return subscriber.Subscribe(x => Console.WriteLine("OK RETURN"));
+        }
+
+        class MyClass
+        {
+            public MyClass(IDisposable d)
+            {
+
+            }
+        }
+
+        static void Subscriber(ISubscriber<int> subscriber)
+        {
+            subscriber.Subscribe(x => Console.WriteLine("NG NOT HANDLED"));
+
+            var d = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN"));
+
+            IDisposable d1;
+            d1 = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN2"));
+
+            _ = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN3"));
+
+            DisposableBag.Create(subscriber.Subscribe(x => Console.WriteLine("OK METHOD ARG1")));
+
+            var bag = DisposableBag.CreateBuilder();
+            subscriber.Subscribe(x => Console.WriteLine("OK METHOD CHAIN")).AddTo(bag);
+
+            Enumerable.Range(1, 10).Select(x => subscriber.Subscribe(y => Console.WriteLine("OK IN LAMBDA")));
+
+            new MyClass(subscriber.Subscribe(x => Console.WriteLine("OK CTOR")));
+
+            using (subscriber.Subscribe(x => Console.WriteLine("OK USING")))
+            {
+            }
+
+            using (var uu = subscriber.Subscribe(x => Console.WriteLine("OK USING2")))
+            {
+            }
+
+            using var u = subscriber.Subscribe(x => Console.WriteLine("OK USING 3"));
+        }
+
+        static void Subscriber2(ISubscriber<string, int> subscriber)
+        {
+            subscriber.Subscribe("a", x => Console.WriteLine("NG NOT HANDLED"));
+
+            var d = subscriber.Subscribe("a", x => Console.WriteLine("OK ASSIGN"));
+
+            IDisposable d1;
+            d1 = subscriber.Subscribe("a", x => Console.WriteLine("OK ASSIGN2"));
+
+            _ = subscriber.Subscribe("a", x => Console.WriteLine("OK ASSIGN3"));
+
+            DisposableBag.Create(subscriber.Subscribe("a", x => Console.WriteLine("OK METHOD ARG1")));
+
+            var bag = DisposableBag.CreateBuilder();
+            subscriber.Subscribe("a", x => Console.WriteLine("OK METHOD CHAIN")).AddTo(bag);
+
+            Enumerable.Range(1, 10).Select(x => subscriber.Subscribe("a", y => Console.WriteLine("OK IN LAMBDA")));
+
+            new MyClass(subscriber.Subscribe("a", x => Console.WriteLine("OK CTOR")));
+
+            using (subscriber.Subscribe("a", x => Console.WriteLine("OK USING")))
+            {
+            }
+
+            using (var uu = subscriber.Subscribe("a", x => Console.WriteLine("OK USING2")))
+            {
+            }
+
+            using var u = subscriber.Subscribe("a", x => Console.WriteLine("OK USING 3"));
+        }
+
+        static void AsyncSubscriber(IAsyncSubscriber<int> subscriber)
+        {
+            subscriber.Subscribe(async (x, ct) => Console.WriteLine("NG NOT HANDLED"));
+
+            var d = subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK ASSIGN"));
+
+            IDisposable d1;
+            d1 = subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK ASSIGN2"));
+
+            _ = subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK ASSIGN3"));
+
+            DisposableBag.Create(subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK METHOD ARG1")));
+
+            var bag = DisposableBag.CreateBuilder();
+            subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK METHOD CHAIN")).AddTo(bag);
+
+            Enumerable.Range(1, 10).Select(x => subscriber.Subscribe(async (y, ct) => Console.WriteLine("OK IN LAMBDA")));
+
+            new MyClass(subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK CTOR")));
+
+            using (subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK USING")))
+            {
+            }
+
+            using (var uu = subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK USING2")))
+            {
+            }
+
+            using var u = subscriber.Subscribe(async (x, ct) => Console.WriteLine("OK USING 3"));
+        }
+
+        static void AsyncSubscriber2(IAsyncSubscriber<string, int> subscriber)
+        {
+            subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("NG NOT HANDLED"));
+
+            var d = subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK ASSIGN"));
+
+            IDisposable d1;
+            d1 = subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK ASSIGN2"));
+
+            _ = subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK ASSIGN3"));
+
+            DisposableBag.Create(subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK METHOD ARG1")));
+
+            var bag = DisposableBag.CreateBuilder();
+            subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK METHOD CHAIN")).AddTo(bag);
+
+            Enumerable.Range(1, 10).Select(async (x, ct) => subscriber.Subscribe("a", async (y, ct) => Console.WriteLine("OK IN LAMBDA")));
+
+            new MyClass(subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK CTOR")));
+
+            using (subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK USING")))
+            {
+            }
+
+            using (var uu = subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK USING2")))
+            {
+            }
+
+            using var u = subscriber.Subscribe("a", async (x, ct) => Console.WriteLine("OK USING 3"));
+        }
+
+        static void BufferedSubscriber(IBufferedSubscriber<int> subscriber)
+        {
+            subscriber.Subscribe(x => Console.WriteLine("NG NOT HANDLED"));
+
+            var d = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN"));
+
+            IDisposable d1;
+            d1 = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN2"));
+
+            _ = subscriber.Subscribe(x => Console.WriteLine("OK ASSIGN3"));
+
+            DisposableBag.Create(subscriber.Subscribe(x => Console.WriteLine("OK METHOD ARG1")));
+
+            var bag = DisposableBag.CreateBuilder();
+            subscriber.Subscribe(x => Console.WriteLine("OK METHOD CHAIN")).AddTo(bag);
+
+            Enumerable.Range(1, 10).Select(x => subscriber.Subscribe(y => Console.WriteLine("OK IN LAMBDA")));
+
+            new MyClass(subscriber.Subscribe(x => Console.WriteLine("OK CTOR")));
+
+            using (subscriber.Subscribe(x => Console.WriteLine("OK USING")))
+            {
+            }
+
+            using (var uu = subscriber.Subscribe(x => Console.WriteLine("OK USING2")))
+            {
+            }
+
+            using var u = subscriber.Subscribe(x => Console.WriteLine("OK USING 3"));
+        }
+    }
+}
---
+++ src/MessagePipe.Analyzer/MessagePipeAnalyzer.cs
@@ -0,0 +1,125 @@
+﻿#pragma warning disable RS2008
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace MessagePipe.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MessagePipeAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            id: "MPA001",
+            title: "MessagePipeAnalyzer001: Don't ignore subscription",
+            messageFormat: "Don't ignore subscription(IDisposable of Subscribe)",
+            category: "Usage",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: "Checks handle Subscribe result(IDisposable).");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var types = new[]
+            {
+                "MessagePipe.ISubscriber`1",
+                "MessagePipe.ISubscriber`2",
+                "MessagePipe.IAsyncSubscriber`1",
+                "MessagePipe.IAsyncSubscriber`2",
+                "MessagePipe.IBufferedSubscriber`1",
+                "MessagePipe.IBufferedAsyncSubscriber`1",
+            };
+            var messagePipeSymbols = types.Select(x => context.SemanticModel.Compilation.GetTypeByMetadataName(x)).Where(x => x != null).ToArray();
+
+            var invocationExpressions = context.Node
+                .DescendantNodes()
+                .OfType<InvocationExpressionSyntax>();
+
+            foreach (var expr in invocationExpressions)
+            {
+                var memberAccess = (expr.Expression as MemberAccessExpressionSyntax);
+                if (memberAccess == null) continue;
+                if (memberAccess.Name.Identifier.ValueText != "Subscribe") continue;
+
+                var callerType = context.SemanticModel.GetTypeInfo(memberAccess.Expression).Type;
+                if (callerType == null) continue;
+                var originalType = callerType.OriginalDefinition;
+                if (originalType == null) continue;
+
+                if (!IsMessagePipeSymbol(messagePipeSymbols!, originalType)) continue;
+
+                if (ValidateInvocation(expr)) continue;
+
+                // Report Warning
+                var diagnostic = Diagnostic.Create(Rule, expr.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        static bool ValidateInvocation(InvocationExpressionSyntax expr)
+        {
+            bool allAncestorsIsParenthes = true;
+            foreach (var x in expr.Ancestors())
+            {
+                // scope is in lambda, method
+                if (x.IsKind(SyntaxKind.SimpleLambdaExpression) || x.IsKind(SyntaxKind.ParenthesizedLambdaExpression) || x.IsKind(SyntaxKind.ArrowExpressionClause))
+                {
+                    // () => M()
+                    if (allAncestorsIsParenthes) return true;
+                    break;
+                }
+                if (x.IsKind(SyntaxKind.MethodDeclaration)) break;
+                if (x.IsKind(SyntaxKind.PropertyDeclaration)) break;
+                if (x.IsKind(SyntaxKind.ConstructorDeclaration)) break;
+
+                // x = M()
+                if (x.IsKind(SyntaxKind.SimpleAssignmentExpression)) return true;
+                // var x = M()
+                if (x.IsKind(SyntaxKind.VariableDeclarator)) return true;
+                // return M()
+                if (x.IsKind(SyntaxKind.ReturnStatement)) return true;
+                // from x in M()
+                if (x.IsKind(SyntaxKind.FromClause)) return true;
+                // (bool) ? M() : M()
+                if (x.IsKind(SyntaxKind.ConditionalExpression)) return true;
+                // M(M())
+                if (x.IsKind(SyntaxKind.InvocationExpression)) return true;
+                // new C(M())
+                if (x.IsKind(SyntaxKind.ObjectCreationExpression)) return true;
+                // using(M())
+                if (x.IsKind(SyntaxKind.UsingStatement)) return true;
+
+                // (((((M()))))
+                if (!x.IsKind(SyntaxKind.ParenthesizedExpression))
+                {
+                    allAncestorsIsParenthes = false;
+                }
+            }
+
+            return false;
+        }
+
+        static bool IsMessagePipeSymbol(INamedTypeSymbol[] messagePipeSymbols, ITypeSymbol typeSymbol)
+        {
+            foreach (var item in messagePipeSymbols)
+            {
+                if (item.Equals(typeSymbol, SymbolEqualityComparer.Default)) return true;
+            }
+
+            return false;
+        }
+    }
+}
